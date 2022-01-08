ee6f01563aa5914b09c4ebf057042b9ac2c0cadc
aljosa <aljosa.kancijanic@gmail.com>
2022-01-08 19:40:28 +0100

    Tested out how to get elements , click them and how to wait for a load to finish before proceeding

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# SeleniumScrapper
-Testing out how to perform scrapping of websites by using Selenium
+# Finding and working with elements
+<br>Trying out the basics of finding elements by their</br>
+<ul>
+    <li>Name</li>
+    <li>ID</li>
+    <li>Progress status</li>
+</ul>
+<br>And doing some basic operations like click()</br>
--- initial_testing.py
+++ initial_testing.py
@@ -5,8 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
-#comment
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Add option to prevent the window from automatically closing after code is executed
 chrome_options = Options()
@@ -15,8 +15,31 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_option
 
 # Site for testing clicks
 driver.get("http://the-internet.herokuapp.com/")
-# Wait until the element loads
+# Wait until the element loads instead of just sleep(time) to save time
 driver.implicitly_wait(10)
-element_new = driver.find_element(By.XPATH, "//*[contains(text(), 'File Download')]")
 
-element_new.click()
+# Click on the option to try out Dynamic loading content
+dynamicLoading_element = driver.find_element(By.XPATH, "//*[contains(text(), 'Dynamic Loading')]")
+dynamicLoading_element.click()
+
+driver.implicitly_wait(10)
+
+# Try out example 2
+dynamicLoading_element_Example2 = driver.find_element(By.XPATH, "//*[contains(text(), 'Example 2: Element rendered after the fact')]")
+dynamicLoading_element_Example2.click()
+
+driver.implicitly_wait(10)
+
+# Find and click the Start button to initiate the loading bar
+dynamicLoading_element_start_btn = driver.find_element(By.XPATH, "//*[contains(text(), 'Start')]")
+dynamicLoading_element_start_btn.click()
+
+# Setup explicit_wait to check when value has finished loading
+
+WebDriverWait(driver, 30).until(
+    # We use the
+    EC.presence_of_element_located((By.ID, 'finish'))
+)
+
+# If we got 'Process finished with exit code 0' that means the Wait worked properly
+# and found the 'Hello World' element that shows up after the loading ends
