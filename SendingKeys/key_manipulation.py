f23ad221e0eb1293c99a5f578574d86a4f32a979
aljosa <aljosa.kancijanic@gmail.com>
2022-01-09 18:28:38 +0100

    Removed unused imports

--- SendingKeys/key_manipulation.py
+++ SendingKeys/key_manipulation.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 # Add option to prevent the window from automatically closing after code is executed
